11e603b6fd3c59bff167ae76dbde14a2bc05ebf4
AlejandroPanizo <98469786+AlejandroPanizo@users.noreply.github.com>
2022-02-21 12:56:06 +0100

    Alejandro Panizo

--- src/main/java/es/codeurjc/helloworldspring/CarritoCompra.java
+++ src/main/java/es/codeurjc/helloworldspring/CarritoCompra.java
@@ -1,5 +1,45 @@
 package es.codeurjc.helloworldspring;
 
-public class CarritoCompra {
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import org.springframework.data.annotation.Id;
+@Entity
+public class CarritoCompra {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@OneToOne
+	private Usuario usuario;
+	@OneToMany
+	private static List<Producto> listaProductos;
+
+	public CarritoCompra() {
+		
+	}
+	public CarritoCompra(Usuario u){
+		this.usuario=u;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	public static List<Producto> getListaProductos() {
+		return listaProductos;
+	}
+	public static void setListaProductos(List<Producto> lista) {
+		listaProductos = lista;
+	}
+	@Override
+	public String toString() {
+		return "CarritoCompra [Usuario=" + usuario + ", listaProductos=" + listaProductos + "]";
+	}
+	
 }
