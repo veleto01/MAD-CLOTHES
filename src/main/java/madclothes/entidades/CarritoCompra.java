f6eac9a59f98e3493effa864aab02390d76e1047
AlejandroPanizo <98469786+AlejandroPanizo@users.noreply.github.com>
2022-03-03 01:07:13 +0100

    Add files via upload

--- src/main/java/madclothes/entidades/CarritoCompra.java
+++ src/main/java/madclothes/entidades/CarritoCompra.java
@@ -1,5 +1,6 @@
 package madclothes.entidades;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -21,7 +22,8 @@ public class CarritoCompra {
 	private Usuario usuario;
 	
 	@OneToMany
-	private static List<Producto> listaProductos;
+	private  List<Producto> listaProductos; 
+
 
 	public CarritoCompra() {
 		
@@ -29,12 +31,13 @@ public class CarritoCompra {
 	//POR DEFECTO CUANDO ENCIENDES LA PAGINA
 	public CarritoCompra(Usuario u){
 		this.usuario=u;
+		this.listaProductos = new ArrayList<>();
 	}
 	//FILTRADO POR USUARIO METIENDO UN PRODUCTO EN LA LISTA DEL CARRITO
 	public CarritoCompra(Usuario u,Producto x){
 		this.usuario=u;
-		
-		
+		this.listaProductos = new ArrayList<>();
+		this.listaProductos.add(x);
 	}
 	
 	
@@ -46,15 +49,20 @@ public class CarritoCompra {
 		this.usuario = usuario;
 	}
 	
-	public static List<Producto> getListaProductos() {
+
+	public  List<Producto> getListaProductos() {
 		return listaProductos;
 	}
-	public static void setListaProductos(List<Producto> lista) {
-		listaProductos = lista;
+
+	
+	public void setListaProductos(List<Producto> listaProductos) {
+		this.listaProductos = listaProductos;
 	}
+	
 	@Override
 	public String toString() {
-		return "CarritoCompra [Usuario=" + usuario + ", listaProductos=" + listaProductos + "]";
+		return "CarritoCompra [Usuario=" + usuario + ", Producto=" + listaProductos + "]";
 	}
+
 	
 }
