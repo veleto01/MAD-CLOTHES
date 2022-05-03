afd426e52f902e1e2d2d6a62cbf8f232113ea4f9
davidsanma2001 <93671942+davidsanma2001@users.noreply.github.com>
2022-05-03 13:15:22 +0200

    Cache controller

---
+++ src/main/java/madclothes/controladores/CacheController.java
@@ -0,0 +1,41 @@
+package madclothes.controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+public class CacheController {
+	@Autowired
+    private CacheManager cacheManager;
+
+    @RequestMapping(value="/cacheProducto", method= RequestMethod.GET)
+    public Map<Object, Object> getCacheReservasContent() {
+        ConcurrentMapCacheManager cacheMgr = (ConcurrentMapCacheManager) cacheManager;
+        ConcurrentMapCache cache = (ConcurrentMapCache) cacheMgr.getCache("Producto");
+        return cache.getNativeCache();
+    }
+
+    @RequestMapping(value="/cacheCarritoCompra", method= RequestMethod.GET)
+    public Map<Object, Object> getCacheSalasContent() {
+        ConcurrentMapCacheManager cacheMgr = (ConcurrentMapCacheManager) cacheManager;
+        ConcurrentMapCache cache = (ConcurrentMapCache) cacheMgr.getCache("CarritoCompra");
+        return cache.getNativeCache();
+    }
+
+    @RequestMapping(value="/cacheWebUser", method= RequestMethod.GET)
+    public Map<Object, Object> getCacheUsersContent() {
+        ConcurrentMapCacheManager cacheMgr = (ConcurrentMapCacheManager) cacheManager;
+        ConcurrentMapCache cache = (ConcurrentMapCache) cacheMgr.getCache("WebUser");
+        return cache.getNativeCache();
+    }
+}
+
+
+
+
