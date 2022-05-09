77f7b046cbc3f370d902613b4643ca963ecabf26
AlejandroPanizo <98469786+AlejandroPanizo@users.noreply.github.com>
2022-05-09 23:50:37 +0200

    Update MadclothesPruebaApplication.java

--- src/main/java/madclothes/MadclothesPruebaApplication.java
+++ src/main/java/madclothes/MadclothesPruebaApplication.java
@@ -7,12 +7,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
 
 @SpringBootApplication
+@EnableHazelcastHttpSession
 public class MadclothesPruebaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MadclothesPruebaApplication.class, args);
 	}
 
+	@Bean
+	public Config config() {
+		
+		Config config = new Config();
+		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+		
+		joinConfig.getMulticastConfig().setEnabled(true);
+		//joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList(environment.getProperty("217.71.204.219")));
+		
+		return config;
+	}
 }
